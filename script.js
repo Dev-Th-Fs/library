bce16889dfa052e41069db15705373b864baf7ad
Talha <talhahussain.splitarts@gmail.com>
2025-02-24 21:31:27 +0500

    created dom nodes dynamically with javascript

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const myLibrary = [];
-const container = document.querySelector(".container")
+const container = document.querySelector(".container");
+
+Book.prototype.changeStatus = function () {
+  book.status = book.status == "read" ? "not read" : "read";
+};
 
 function Book(author, title, pages, status) {
   this.author = author;
@@ -13,24 +17,46 @@ function addBookToLibrary(author, title, pages, status) {
   myLibrary.push(newBook);
 }
 
-addBookToLibrary('abc','xyz', 12, 'not read');
-console.table(myLibrary)
+addBookToLibrary("abc", "xyz", 12, "not read");
 
-function createDomNodes(author){
+function createDomNodes(book) {
+  const data = [
+    `Author: ${book.author}`,
+    `Title: ${book.title}`,
+    `Pages: ${book.pages}`,
+    `Status: ${book.status}`,
+  ];
+  console.table(data);
   const cardContainer = document.createElement("div");
-  cardContainer.classList.add("card")
-  for(let i=1; i<=4; i++){
-    const para = document.createElement("p");
-    para.textContent = `Author: ${author}`
-    container.appendChild(para)
-  }
+  cardContainer.classList.add("card");
+  
+  for (let i = 0; i < 4; i++) {
+    if (i == 3) {
+      const statusContainer = document.createElement("div");
+      const para = document.createElement("p");
+      const statusButton = document.createElement("button");
 
+      statusContainer.classList.add("statusContainer");
+      statusButton.classList.add("statusButton");
+
+      para.textContent = ` ${data[i]}`;
+      statusButton.textContent = "X";
+
+      statusContainer.append(para, statusButton);
+      cardContainer.appendChild(statusContainer);
+    } else {
+      const para = document.createElement("p");
+      para.textContent = ` ${data[i]}`;
+      cardContainer.appendChild(para);
+    }
+  }
+  container.appendChild(cardContainer);
 }
 
-function display(){
-  myLibrary.forEach(book => {
-    createDomNodes(book.author)
+function display() {
+  myLibrary.forEach((book) => {
+    createDomNodes(book);
   });
 }
 
-display()
+display();
